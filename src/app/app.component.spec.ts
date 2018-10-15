04429b0010db3e46e13862650cb62ab110f3c84e
Meelistenso <meelistenso@gmail.com>
2018-10-15 22:49:07 +0300

    fix(tests): clear

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,7 +6,6 @@ import {TranslateModule} from '@ngx-translate/core';
 import {AppRoutingModule} from './app-routing.module';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {APP_CONFIG, AppConfig} from './config/app.config';
-import {HeroService} from './modules/heroes/shared/hero.service';
 import {CoreModule} from './core/core.module';
 
 describe('AppComponent', () => {
@@ -27,7 +26,6 @@ describe('AppComponent', () => {
       providers: [
         {provide: APP_CONFIG, useValue: AppConfig},
         {provide: APP_BASE_HREF, useValue: '/'},
-        HeroService
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
@@ -43,7 +41,7 @@ describe('AppComponent', () => {
 
   it('should change title meta tag in root path', async(() => {
     fixture.detectChanges();
-    expect(component.title.getTitle()).toBe('Angular Example App');
+    expect(component.title.getTitle()).toBe('XTest 3');
   }));
 
   it('should check browser features', (() => {
