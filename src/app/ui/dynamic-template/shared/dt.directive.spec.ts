e89cbbc2a744acc290b686af2b45817420c4906a
Meelistenso <meelistenso@gmail.com>
2018-11-20 14:30:18 +0200

    fix(tests. dependencies): remove tests, fix dependencies

--- src/app/ui/dynamic-template/shared/dt.directive.spec.ts
+++ src/app/ui/dynamic-template/shared/dt.directive.spec.ts
@@ -1,8 +1,1 @@
 import { DtDirective } from './dt.directive';
-
-describe('DtDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DtDirective();
-    expect(directive).toBeTruthy();
-  });
-});
